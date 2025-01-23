92a75f8a4f2fdfc5ea194ddc33599624d3a7c8a0
KGxALIHAN <ulugbekovalihan1@gmail.com>
2025-01-23 09:07:33 -0500

    сделал дз 3

--- afisha/movie_app/urls.py
+++ afisha/movie_app/urls.py
@@ -1,20 +1,26 @@
 from django.urls import path
 from .views import (
     DirectorListAPIView,
+    DirectorCreateAPIView,
     DirectorDetailAPIView,
     MovieListAPIView,
+    MovieCreateAPIView,
     MovieDetailAPIView,
-    ReviewListAPIView,
-    ReviewDetailAPIView,
     MoviesWithReviewsAPIView,
+    ReviewListAPIView,
+    ReviewCreateAPIView,
+    ReviewDetailAPIView,
 )
 
 urlpatterns = [
     path('directors/', DirectorListAPIView.as_view(), name='director-list'),
+    path('directors/create/', DirectorCreateAPIView.as_view(), name='director-create'),
     path('directors/<int:pk>/', DirectorDetailAPIView.as_view(), name='director-detail'),
     path('movies/', MovieListAPIView.as_view(), name='movie-list'),
+    path('movies/create/', MovieCreateAPIView.as_view(), name='movie-create'),
     path('movies/<int:pk>/', MovieDetailAPIView.as_view(), name='movie-detail'),
     path('movies/reviews/', MoviesWithReviewsAPIView.as_view(), name='movies-with-reviews'),
     path('reviews/', ReviewListAPIView.as_view(), name='review-list'),
+    path('reviews/create/', ReviewCreateAPIView.as_view(), name='review-create'),
     path('reviews/<int:pk>/', ReviewDetailAPIView.as_view(), name='review-detail'),
 ]
--- afisha/movie_app/views.py
+++ afisha/movie_app/views.py
@@ -1,66 +1,59 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.generics import ListAPIView, RetrieveAPIView
-from .models import Director, Movie, Review
+from rest_framework.generics import ListAPIView, CreateAPIView, RetrieveUpdateDestroyAPIView
+from .models import Director, Movie, Review 
 from .serializers import DirectorSerializer, MovieSerializer, ReviewSerializer
 
-# Список режиссёров
 class DirectorListAPIView(APIView):
     def get(self, request):
         directors = Director.objects.all()
-        data = []
-        for director in directors:
-            data.append({
-                "id": director.id,
-                "name": director.name,
-                "movies_count": director.movies.count()  # Подсчёт фильмов
-            })
+        data = [{"id": director.id, "name": director.name, "movies_count": director.movies.count()} for director in directors]
         return Response(data)
 
-
-# Детальная информация о режиссёре
-class DirectorDetailAPIView(RetrieveAPIView):
+class DirectorCreateAPIView(CreateAPIView):
     queryset = Director.objects.all()
     serializer_class = DirectorSerializer
 
+class DirectorDetailAPIView(RetrieveUpdateDestroyAPIView):
+    queryset = Director.objects.all()
+    serializer_class = DirectorSerializer
 
-# Список фильмов
 class MovieListAPIView(ListAPIView):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
 
-
-# Детальная информация о фильме
-class MovieDetailAPIView(RetrieveAPIView):
+class MovieCreateAPIView(CreateAPIView):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
 
+class MovieDetailAPIView(RetrieveUpdateDestroyAPIView):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
 
-# Список фильмов с отзывами и средней оценкой
 class MoviesWithReviewsAPIView(APIView):
     def get(self, request):
         movies = Movie.objects.all()
-        data = []
-        for movie in movies:
-            data.append({
-                "id": movie.id,
-                "title": movie.title,
-                "description": movie.description,
-                "duration": movie.duration,
-                "director": movie.director.name,
-                "reviews": ReviewSerializer(movie.reviews.all(), many=True).data,
-                "rating": movie.average_rating()  # Средний рейтинг фильма
-            })
+        data = [{
+            "id": movie.id,
+            "title": movie.title,
+            "description": movie.description,
+            "duration": movie.duration,
+            "director": movie.director.name,
+            "reviews": ReviewSerializer(movie.reviews.all(), many=True).data,
+            "rating": movie.average_rating()
+        } for movie in movies]
         return Response(data)
 
-
-# Список отзывов
 class ReviewListAPIView(ListAPIView):
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
 
-
-# Детальная информация об отзыве
-class ReviewDetailAPIView(RetrieveAPIView):
+class ReviewCreateAPIView(CreateAPIView):
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
+
+class ReviewDetailAPIView(RetrieveUpdateDestroyAPIView):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+class re():
+    pass
